aebe3ba6553f038984b3ee6c969814448b039285
Benjamin Welzel <b.welzel81@gmail.com>
2021-07-07 15:10:52 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const App = () => {
+    return (
+        <div className = "h-screen bg-gray-200 p-8 flex justify-center">
+            <h1 className = "text-2xl font-bold">Hello from React and TailwindCSS!</h1>
+        </div>
+    )
+}
+
+export default App
