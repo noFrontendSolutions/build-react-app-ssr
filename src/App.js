53682f4128e989ad591e3063afed5bfebdb0f455
Benjamin Welzel <b.welzel81@gmail.com>
2021-07-07 19:01:07 +0200

    added Tailwind jit compilation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react"
 const App = () => {
     return (
         <div className = "h-screen bg-gray-200 p-8 flex justify-center">
-            <h1 className = "text-2xl font-bold">Hello from React and TailwindCSS!</h1>
+            <h1 className = "text-3xl font-bold text-gray-800">Greetings from React and TailwindCSS!</h1>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ['./src/*.html', './dist/*.html'],
+  mode: "jit",
+  purge: ['./src/*.{html,js,ts,jsx,tsx}', './dist/*.{html,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
