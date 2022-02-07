e9ba2dbb48244ce7730739f7aa9e70dde4105e01
noFrontendSolutions <b.welzel81@gmail.com>
2022-02-07 14:20:07 +0100

    updated node packages and made some config changes (according to Tailwind version 3 standards)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-  mode: "jit",
-  purge: ['./src/**/*.html',
+  content: ['./src/**/*.html',
           './src/**/*.js',
           './src/**/*.ts',
           './src/**/*.jsx',
           './src/**/*.tsx'
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
