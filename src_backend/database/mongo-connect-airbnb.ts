46db90006e1f3043c2b2e54c814105fea680188b
noFrontendSolutions <b.welzel81@gmail.com>
2022-02-12 00:08:03 +0100

    removed all semicolons with custom 'prettier' configuration

--- src_backend/database/mongo-connect-airbnb.ts
+++ src_backend/database/mongo-connect-airbnb.ts
@@ -1,18 +1,22 @@
-import 'dotenv/config'
-import * as mongoDB from "mongodb";
+import "dotenv/config"
+import * as mongoDB from "mongodb"
 
 const initialConnectionAirbnb = async (client: mongoDB.MongoClient) => {
-  const dbName: string = process.env.DB_NAME || "";
-  const dbCollection: string = process.env.DB_COLLECTION || "";
-  await client.connect();
-  const db: mongoDB.Db = client.db(dbName);
-  const collection: mongoDB.Collection = db.collection(dbCollection);
+  const dbName: string = process.env.DB_NAME || ""
+  const dbCollection: string = process.env.DB_COLLECTION || ""
+  await client.connect()
+  const db: mongoDB.Db = client.db(dbName)
+  const collection: mongoDB.Collection = db.collection(dbCollection)
   const results = await collection
-    //passes back the first ten results of adresses that are located in Manhatten, with "picture_url" and "summary" not empty strings 
-    .find({ "address.suburb": "Manhattan", "images.picture_url": {$ne: ""}, "summary": {$ne: ""}})
+    //passes back the first ten results of adresses that are located in Manhatten, with "picture_url" and "summary" not empty strings
+    .find({
+      "address.suburb": "Manhattan",
+      "images.picture_url": { $ne: "" },
+      summary: { $ne: "" },
+    })
     .limit(10)
-    .toArray();
-  return results;
-};
+    .toArray()
+  return results
+}
 
-export = { initialConnectionAirbnb };
+export = { initialConnectionAirbnb }
--- src_backend/server.ts
+++ src_backend/server.ts
@@ -1,33 +1,29 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express"
 import express from "express"
 import cors from "cors"
-import {MongoClient} from "mongodb"
-import mongoConnectAirbnb from "./database/mongo-connect-airbnb" 
-import * as mongoDB from "mongodb";
-import 'dotenv/config'
+import { MongoClient } from "mongodb"
+import mongoConnectAirbnb from "./database/mongo-connect-airbnb"
+import * as mongoDB from "mongodb"
+import "dotenv/config"
 
-
-
-const url: string  = process.env.MONGO_URI || ""
-const port: number | string = process.env.PORT || 3000;
+const url: string = process.env.MONGO_URI || ""
+const port: number | string = process.env.PORT || 3000
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 app.get("/airbnb", async (req: Request, res: Response) => {
-    const client: mongoDB.MongoClient = new MongoClient(url);
-    try {
+  const client: mongoDB.MongoClient = new MongoClient(url)
+  try {
     const initialLoad = await mongoConnectAirbnb.initialConnectionAirbnb(client)
     await client.close()
     res.send(initialLoad)
-    }
-    catch (err) {
+  } catch (err) {
     console.log(err)
-    }
-    
+  }
 })
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
+  console.log(`Server listening on port ${port}`)
 })
